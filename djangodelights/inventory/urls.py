85b8412fb41b626b4ceae3a7572061728f7b37af
Amaury Peralta <amauryperalta28@gmail.com>
2024-11-27 17:10:51 -0400

    Add new side bar views

--- djangodelights/inventory/urls.py
+++ djangodelights/inventory/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('ingredients/', views.ingredients, name='ingredients'),
+    path('menu/', views.menu, name='menu'),
+    path('purchases/', views.purchases, name='purchases'),
+    path('profit_revenue/', views.profit_revenue, name='profit_revenue'),
 ]
--- djangodelights/inventory/views.py
+++ djangodelights/inventory/views.py
@@ -4,3 +4,15 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'inventory/index.html')
+
+def ingredients(request):
+    return render(request, 'inventory/ingredients.html')
+
+def menu(request):
+    return render(request, 'inventory/menu.html')
+
+def purchases(request):
+    return render(request, 'inventory/purchases.html')
+
+def profit_revenue(request):
+    return render(request, 'inventory/profit_revenue.html')
