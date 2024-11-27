7ff14fc5a4fe204b2fc7d2a1af2d03ed37035fcf
Amaury Peralta <amauryperalta28@gmail.com>
2024-11-27 16:56:19 -0400

    Add missing migrations Adjust template to django dlights inventory system

--- djangodelights/inventory/models.py
+++ djangodelights/inventory/models.py
@@ -9,15 +9,15 @@ class Ingredient(models.Model):
     cost = models.FloatField()
 
 class MenuItem(models.Model):
-    name = models.CharField(max_length=30)
+    title = models.CharField(max_length=30)
     price = models.FloatField(null=False, blank=False)
 
 class RecipeRequirement(models.Model):
-    menu_item_id = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
-    Ingredient_Id = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    menuitem = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
     quantity = models.IntegerField(default=1)
 
 class Purchase(models.Model):
     customer_name = models.CharField(max_length=30)
     MenuItemId = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
-    date = models.DateField()
+    date = models.DateTimeField()
