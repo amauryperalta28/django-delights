aeebf9a93fb67d04ea941fb8b884f222e2f25d4d
Amaury Peralta <amauryperalta28@gmail.com>
2024-12-03 12:13:45 -0400

    Customized update ingredient form

---
+++ djangodelights/inventory/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from .models import Ingredient
+
+class IngredientForm(forms.ModelForm):
+    class Meta:
+        model = Ingredient
+        fields = ['name', 'price', 'unit', 'quantity', 'cost']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Ingredient name', 'id': 'id_name'}),
+            'price': forms.TextInput(attrs={'class': 'form-control', 'aria-label': 'Price (to the nearest dollar)', 'id': 'id_amount'}),
+            'unit': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Ingredient unit', 'id': 'id_unit'}),
+            'quantity': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Ingredient quantity', 'id': 'id_quantity'}),
+            'cost': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Ingredient cost', 'id': 'id_cost'}),
+        }
