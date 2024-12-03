961235a376c2ec753a46aa5b3496983cca99a6a1
Amaury Peralta <amauryperalta28@gmail.com>
2024-12-03 10:43:53 -0400

    Connect ingredient list template to database

--- djangodelights/inventory/urls.py
+++ djangodelights/inventory/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('ingredients/', views.ingredients_list, name='ingredients'),
+    path('ingredients/', views.IngredientsListView.as_view(), name='ingredients'),
     path('menu/', views.menuitem_list, name='menu'),
     path('purchases/', views.purchases_list, name='purchases'),
     path('profit_revenue/', views.profit_revenue, name='profit_revenue'),
--- djangodelights/inventory/views.py
+++ djangodelights/inventory/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Ingredient
 
 # Create your views here.
+
+class IngredientsListView(ListView):
+    model = Ingredient
+    template_name = 'inventory/ingredient_list.html'
+    context_object_name = 'ingredient_list'
+    
 
 def index(request):
     return render(request, 'inventory/index.html')
