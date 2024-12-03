359527c1ba53662a904daf50f80b56ffe7b63024
Amaury Peralta <amauryperalta28@gmail.com>
2024-12-03 10:20:16 -0400

    Add new html to some templates Rename some templates

--- djangodelights/inventory/views.py
+++ djangodelights/inventory/views.py
@@ -6,10 +6,10 @@ def index(request):
     return render(request, 'inventory/index.html')
 
 def ingredients_list(request):
-    return render(request, 'inventory/ingredients.html')
+    return render(request, 'inventory/ingredient_list.html')
 
 def menuitem_list(request):
-    return render(request, 'inventory/menu.html')
+    return render(request, 'inventory/menu_item_list.html')
 
 def purchases_list(request):
     return render(request, 'inventory/purchases.html')
