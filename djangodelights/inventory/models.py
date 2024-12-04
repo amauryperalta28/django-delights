9d91b3e98706e6d8a5d860325a7bb83da744e54f
Amaury Peralta <amauryperalta28@gmail.com>
2024-12-04 12:05:20 -0400

    Refactor models

--- djangodelights/inventory/models.py
+++ djangodelights/inventory/models.py
@@ -7,10 +7,16 @@ class Ingredient(models.Model):
     unit = models.CharField(max_length=8)
     quantity = models.IntegerField(default=1)
     cost = models.FloatField()
+        
+    def __str__(self):
+        return self.name
 
 class MenuItem(models.Model):
     title = models.CharField(max_length=30)
     price = models.FloatField(null=False, blank=False)
+        
+    def __str__(self):
+        return self.title
 
 class RecipeRequirement(models.Model):
     menuitem = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
@@ -21,3 +27,6 @@ class Purchase(models.Model):
     customer_name = models.CharField(max_length=30)
     MenuItemId = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
     date = models.DateTimeField()
+    
+    def __str__(self):
+        return self.MenuItemId.title
