d14e6227db9f7e217a0cf14f38d266c228106146
Amaury Peralta <amauryperalta28@gmail.com>
2024-12-06 11:19:59 -0400

    Refactor urls

--- djangodelights/inventory/urls.py
+++ djangodelights/inventory/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.CustomLoginView.as_view(), name='login'),
     path('login/', views.CustomLoginView.as_view(), name='login'),
     path('logout/', views.custom_logout, name='logout'),
     path('dashboard/', views.index, name='dashboard'),
